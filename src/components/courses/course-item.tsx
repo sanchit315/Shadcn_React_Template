38aadab8fb45443c1f81fe917f4fb8447ec8af2c
Sanchit Bhadgal <sanchitbhadgal@Sanchits-MacBook-Pro.local>
2025-02-01 06:27:45 +0530

    fix

--- src/components/courses/course-item.tsx
+++ src/components/courses/course-item.tsx
@@ -11,6 +11,14 @@ const CourseCard: React.FC<CourseCardProps> = ({
   description,
   onClick,
 }) => {
+  const images = [
+    "https://dme2wmiz2suov.cloudfront.net/User(5345174)/CourseBundles(23210)/1849061-Crash.png",
+    "https://www.ox.ac.uk/sites/files/oxford/Choosing-an-Oxford-course.jpg",
+    "https://cdni.iconscout.com/illustration/premium/thumb/course-rating-illustration-download-in-svg-png-gif-file-formats--like-logo-education-review-e-learning-pack-school-illustrations-5127250.png?f=webp",
+  ];
+
+  const randomIndex = Math.floor(Math.random() * 3);
+
   return (
     <div
       className="rounded-lg overflow-hidden shadow-md border border-gray-300 cursor-pointer"
@@ -18,7 +26,7 @@ const CourseCard: React.FC<CourseCardProps> = ({
     >
       <img
         className="w-full h-48 object-cover"
-        src="/image.png"
+        src={images[randomIndex]}
         alt="Card Image"
       />
       <div className="py-4 px-2">
